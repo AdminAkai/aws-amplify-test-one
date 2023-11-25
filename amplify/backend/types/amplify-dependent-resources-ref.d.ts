07a597afb02610857a7fbdabd2aabbf291d178ac
AdminAkai <akatoirotech@gmail.com>
2023-11-25 04:14:21 -0500

    added amplify authenticator and react ui

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    awsamplifytestonedf918269: {
+      AppClientID: 'string'
+      AppClientIDWeb: 'string'
+      IdentityPoolId: 'string'
+      IdentityPoolName: 'string'
+      UserPoolArn: 'string'
+      UserPoolId: 'string'
+      UserPoolName: 'string'
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
+import { FC } from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { ThemeProvider } from 'styled-components'
 
-import { selectMode } from 'src/shared/redux/settingsSlice/selectors'
-import { useAppSelector } from 'src/shared/redux/store'
+// import { selectMode } from 'src/shared/redux/settingsSlice/selectors'
+// import { useAppSelector } from 'src/shared/redux/store'
 
-import Main from 'src/features/Main'
-import Landing from 'src/features/Landing'
+// import Main from 'src/features/Main'
+// import Landing from 'src/features/Landing'
 
-import theme from './shared/lib/theme'
+// import theme from './shared/lib/theme'
 
 import './assets/fonts/stylesheet.css'
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  View,
+  Card,
+} from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
 
-import GlobalStyle from './shared/lib/globalStyles'
+// import GlobalStyle from './shared/lib/globalStyles'
 import './App.css'
+import '@aws-amplify/ui-react/styles.css'
 
-function App() {
-  const mode = useAppSelector(selectMode)
+Amplify.configure(config)
+
+const App: FC<{ signOut: any }> = ({ signOut }) => {
+  // const mode = useAppSelector(selectMode)
 
   return (
-    <ThemeProvider theme={theme[mode]}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route path='/' element={<Main />}>
-            <Route index element={<Landing />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <View className='App'>
+      <Card>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   )
+
+  // return (
+  //   <ThemeProvider theme={theme[mode]}>
+  //     <BrowserRouter>
+  //       <GlobalStyle />
+  //       <Routes>
+  //         <Route path='/' element={<Main />}>
+  //           <Route index element={<Landing />} />
+  //         </Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </ThemeProvider>
+  // )
 }
 
-export default App
+export default withAuthenticator(App)
