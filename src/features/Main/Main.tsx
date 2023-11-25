20bdc4f03d0385e4325426b4125ea00a4a7c681d
AdminAkai <akatoirotech@gmail.com>
2023-11-24 23:07:53 -0500

    adjusted readme, fixed import in Main.tsx

--- src/features/Main/Main.tsx
+++ src/features/Main/Main.tsx
@@ -1,14 +1,11 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Navbar from 'src/shared/components/Navbar'
-
 import { MainContainer } from './styledComponents'
 
 const Main: FC = () => {
   return (
     <MainContainer>
-      <Navbar />
       <Outlet />
     </MainContainer>
   )
